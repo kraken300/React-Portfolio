7a89683164411f6b02a567fcde31a277baedaf70
kraken300 <shivamsingh113355@gmail.com>
2025-08-02 23:17:44 +0530

    Add basic portfolio structure

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Project from './components/Project';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <About />
+      <Skills />
+      <Project />
+      <Contact />
+      <Footer />
+    </div>
+
+  );
+};
+
+export default App;
